48788e981581901cf5f7a06ef28bc45f817b2680
Leonam Rabelo <rabeloleonam@gmail.com>
2025-04-16 03:26:03 -0300

    first commit

---
+++ CameraSeguranca.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/uart.h"
+
+#define I2C_PORT i2c0
+#define I2C_SDA 8
+#define I2C_SCL 9
+
+#include "blink.pio.h"
+
+int main(){
+    
+    while(true){
+
+    }
+    return 0;
+}
